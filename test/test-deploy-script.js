6d1d9e247da1e08cba3284634f31cfa4973ccfa3
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2015-03-17 18:31:12 -0700

    more useful test script

--- test/test-deploy-script.js
+++ test/test-deploy-script.js
@@ -1,1 +1,7 @@
 console.log('running this code on V2!');
+
+var fs = require('fs');
+fs.writeFile('/sys/class/leds/tessel\:blue\:user2/trigger', 'timer', function (err) {
+  if(err) throw err;
+  console.log('Blue light should be blinking. To turn it off, run echo none > /sys/class/leds/tessel\:blue\:user2/trigger on the Tessel.');
+});
