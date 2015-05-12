03d9222116f1ce3661c6ce807d9234aa4608861a
Kenneth Nierenhausen <kennethnierenhausen@gmail.com>
2015-05-12 15:52:57 -0700

    Changed over to --lang rust. Now initializes in the current directory

--- bin/tessel-2.js
+++ bin/tessel-2.js
@@ -156,7 +156,7 @@ parser.command('init')
     position: 1,
     help: 'Provide a directory to initialize your project'
   })
-  .help('Initialize repository for your Tessel project')
+g  .help('Initialize repository for your Tessel project')
 
 parser.command('wifi')
   .callback(function(opts) {
