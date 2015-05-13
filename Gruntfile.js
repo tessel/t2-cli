8b2be5326f594422054d2c6271a3386f9af9ad4b
Rick Waldron <waldron.rick@gmail.com>
2015-05-13 17:46:14 -0400

    grunt, grunt-cli, basic code maintenance tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      tests: [
+        'test/unit/*.js'
+      ]
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'bin/*',
+        'lib/**/*.js',
+        'test/**/*.js',
+        'Gruntfile.js',
+
+        // This is commented out because there are
+        // too many errors to address. I went through
+        // half of them and still:
+        //
+        // >> 75 errors in 3 files
+        //
+        // There are a lot of undefined vars being used
+        // and without fully understanding these files, it's
+        // not worth the effort to fix them all.
+        // 'resources/**/*.js',
+      ]
+    },
+    jscs: {
+      all: [
+        'bin/*',
+        'lib/**/*.js',
+        'test/**/*.js',
+        'Gruntfile.js',
+        // 'resources/**/*.js',
+      ],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+    jsbeautifier: {
+      all: [
+        'bin/*',
+        'lib/**/*.js',
+        'test/**/*.js',
+        'Gruntfile.js',
+        // 'resources/**/*.js',
+      ],
+      options: {
+        mode: 'VERIFY_ONLY',
+        braceStyle: 'collapse',
+        breakChainedMethods: false,
+        e4x: false,
+        evalCode: false,
+        indentChar: ' ',
+        indentLevel: 0,
+        indentSize: 2,
+        indentWithTabs: false,
+        jslintHappy: false,
+        keepArrayIndentation: false,
+        keepFunctionIndentation: false,
+        maxPreserveNewlines: 10,
+        preserveNewlines: true,
+        spaceBeforeConditional: true,
+        spaceInParen: false,
+        unescapeStrings: false,
+        wrapLineLength: 0
+      }
+    },
+    watch: {
+      src: {
+        files: [
+          'Gruntfile.js',
+          'lib/**/!(johnny-five)*.js',
+          'test/**/*.js',
+          'eg/**/*.js'
+        ],
+        tasks: ['default'],
+        options: {
+          interrupt: true,
+        },
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.loadNpmTasks('grunt-nodeunit');
+
+  // 'npm test' runs these tasks
+  grunt.registerTask('test', ['jshint']);
+
+  // Default task.
+  grunt.registerTask('default', ['test']);
+
+};
