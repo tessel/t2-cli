c1b6835665941383b19f2bfeb44acee77ca8bfcd
Rick Waldron <waldron.rick@gmail.com>
2015-05-13 17:49:35 -0400

    grunt-jsbeautifier: fix options

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,23 +50,25 @@ module.exports = function(grunt) {
         // 'resources/**/*.js',
       ],
       options: {
-        braceStyle: 'collapse',
-        breakChainedMethods: false,
-        e4x: false,
-        evalCode: false,
-        indentChar: ' ',
-        indentLevel: 0,
-        indentSize: 2,
-        indentWithTabs: false,
-        jslintHappy: false,
-        keepArrayIndentation: false,
-        keepFunctionIndentation: false,
-        maxPreserveNewlines: 10,
-        preserveNewlines: true,
-        spaceBeforeConditional: true,
-        spaceInParen: false,
-        unescapeStrings: false,
-        wrapLineLength: 0
+        js: {
+          braceStyle: "collapse",
+          breakChainedMethods: false,
+          e4x: false,
+          evalCode: false,
+          indentChar: " ",
+          indentLevel: 0,
+          indentSize: 2,
+          indentWithTabs: false,
+          jslintHappy: false,
+          keepArrayIndentation: false,
+          keepFunctionIndentation: false,
+          maxPreserveNewlines: 10,
+          preserveNewlines: true,
+          spaceBeforeConditional: true,
+          spaceInParen: false,
+          unescapeStrings: false,
+          wrapLineLength: 0
+        }
       }
     },
     watch: {
