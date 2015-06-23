6435270b98551a7e73fc2f679d3184ad50ceea24
jialiya <jialiya.huang0@gmail.com>
2015-06-23 10:13:02 +0800

    usb attach & deattach events

--- test.js
+++ test.js
@@ -48,23 +48,34 @@ var foundTessels = [];
 // list tessels as they attach and detach
 function findTessels() {
   var seeker = new t2.discover.TesselSeeker().start();
+  seeker.on('usbConnection', function(tessel){
+    // got a usb connection to a tessel
+    console.log("usb connected", tessel.connection.serialNumber, tessel.name);
+  })
+
   seeker.on('tessel', function(tessel) {
     if (tessel.connection.connectionType == 'USB') {
+      // found the name of the tessel
       // add only if tessels are over usb
-      console.log("tessel", tessel.name);
+      // console.log("tessel", tessel.name);
       foundTessels.push({serialNumber: tessel.connection.serialNumber,
         name: tessel.name});
       console.log("foundTessels", seeker.usbDeviceList);
+      tessel.connection.end(function(){
+        console.log('ending the connection of ', tessel.name);
+      })
     }
   });
 
   seeker.on('detach', function (tessel){
     // go through list of tessels and remove the detached device
-    console.log("detaching", tessel.name);
-      console.log("foundTessels", seeker.usbDeviceList);
-
+    console.log("detaching", tessel.connection.serialNumber);
+    console.log("foundTessels", seeker.usbDeviceList);
     // console.log("foundTessels", foundTessels);
   });
 }
 
-findTessels();
+console.log("finding after timeout");
+// setTimeout(function(){
+  findTessels();
+// }, 20*1000);
