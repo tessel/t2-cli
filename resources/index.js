53f5e54fe405df633081f2e493c960f585387ba5
Rick Waldron <waldron.rick@gmail.com>
2015-10-26 16:31:10 -0400

    Only display the blinking and quitting message once

--- resources/index.js
+++ resources/index.js
@@ -6,7 +6,8 @@ tessel.led[2].on();
 
 // Blink!
 setInterval(function () {
-  console.log("I'm blinking! (Press CTRL + C to stop)");
   tessel.led[2].toggle();
   tessel.led[3].toggle();
 }, 100);
+
+console.log("I'm blinking! (Press CTRL + C to stop)");
