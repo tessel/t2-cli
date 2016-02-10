8d7e1a95d3c34d4c70bbc2a181fe678010e39a4c
johnnyman727 <johnnyman727@gmail.com>
2016-02-10 14:39:21 -0800

    Export Tessel object for composition

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var controller = require('./lib/controller');
 var commands = require('./lib/tessel/commands');
+var Tessel = require('./lib/tessel/tessel');
+
 module.exports = controller;
 module.exports.commands = commands;
+module.exports.Tessel = Tessel;
