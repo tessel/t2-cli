6beaa3f8e27ef757efccb9e6bab800ff439c650a
johnnyman727 <johnnyman727@gmail.com>
2016-02-11 16:08:11 -0800

    Export USB and LAN connections

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var controller = require('./lib/controller');
-var commands = require('./lib/tessel/commands');
-var Tessel = require('./lib/tessel/tessel');
-
-module.exports = controller;
-module.exports.commands = commands;
-module.exports.Tessel = Tessel;
+// The controller is used for standard Tessel commands
+// like Tessel.get, Tessel.list, Tessel.deploy, etc.
+module.exports = require('./lib/controller');
+// Commands give deeper access to all the standard shell commands
+// that we run to execute larger sequences
+module.exports.commands = require('./lib/tessel/commands');
+// Exporting Tessel allows consumers to compose their own
+// functions onto the prototype
+module.exports.Tessel = require('./lib/tessel/tessel');
+// The seeker allows consumers to have a long running discovery
+// process and take action as Tessels are connected/disconnected
+module.exports.discovery = require('./lib/discover').TesselSeeker;
+// The USBConnection libray lets consumers turn arbitrary USB devices
+// into USBConnection objects to be used in Tessel creation.
+module.exports.USBConnection = require('./lib/usb_connection').USB.Connection;
+// The LANConnection libray lets consumers turn arbitrary LAN devices
+// into LANConnection objects to be used in Tessel creation.
+module.exports.LANConnection = require('./lib/lan_connection');
