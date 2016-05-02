282562d485d06f2ac7ae435eee2e3752cb200b05
Matt Paine <matt@mattsoftware.com>
2016-05-02 10:50:03 +1000

    Added unit tests, changed indexOf to includes (thanks @rwaldron)

--- test/unit/lan-connection.js
+++ test/unit/lan-connection.js
@@ -21,6 +21,44 @@ exports['LAN.Connection'] = {
     test.equal(this.lanConnection.connectionType, 'LAN');
     test.done();
   },
+
+  ipWhenIpV6: function(test) {
+    this.lanConnection = new LAN.Connection({
+      addresses: ['fc00::', '172.16.2.5'],
+      host: 'home.loc',
+    });
+    test.equal(this.lanConnection.ip, 'fc00::');
+    test.done();
+  },
+
+  ipWhenIpV6LinkLocal: function(test) {
+    this.lanConnection = new LAN.Connection({
+      addresses: ['fc00::', '172.16.2.5'],
+      networkInterface: 'en0',
+      host: 'home.loc',
+    });
+    test.equal(this.lanConnection.ip, 'fc00::%en0');
+    test.done();
+  },
+
+  ipWhenIpV4: function(test) {
+    this.lanConnection = new LAN.Connection({
+      addresses: ['172.16.2.5', 'fc00::'],
+      networkInterface: 'en0',
+      host: 'home.loc',
+    });
+    test.equal(this.lanConnection.ip, '172.16.2.5');
+    test.done();
+  },
+
+  ipWhenNoIps: function(test) {
+    this.lanConnection = new LAN.Connection({
+      networkInterface: 'en0',
+      host: 'home.loc',
+    });
+    test.equal(this.lanConnection.ip, 'home.loc');
+    test.done();
+  },
 };
 
 exports['LAN.Scanner'] = {
