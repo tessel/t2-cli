f6bb140173c64cca0ec1b332d8506d1182d7082f
Rick Waldron <waldron.rick@gmail.com>
2016-06-24 21:16:22 -0400

    t2 init: broad cleanup (#779)
    * t2 init: broad cleanup
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    
    * t2 init: more tests for --lang=javascript
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    
    * t2 init: per review: not necessary to explicitly coerce lang input to string
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    
    * t2 init: init.rs.createSampleProgram use fs.copy
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    
    * t2 init: special *Error classes are not exported.
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    
    * t2 init: reuse rust resource paths
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    
    * t2 init: cleanup all resource paths
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- test/unit/init.js
+++ test/unit/init.js
@@ -1,77 +1,63 @@
-exports['init (language args)'] = {
+exports['init.resolveLanguage()'] = {
   setUp: function(done) {
     done();
   },
   tearDown: function(done) {
     done();
   },
-  javascriptArgs: function(test) {
+  javascript: function(test) {
     test.expect(4);
     // No language arg indicates JavaScript by default
-    test.ok(init.resolveLanguage({}) === init.js);
+    test.equal(init.resolveLanguage(), init.js);
     // Can request JavaScript with explicit name
-    test.ok(init.resolveLanguage({
-      lang: 'javascript'
-    }) === init.js);
+    test.equal(init.resolveLanguage('javascript'), init.js);
     // Can request JavaScript with abbr
-    test.ok(init.resolveLanguage({
-      lang: 'js'
-    }) === init.js);
+    test.equal(init.resolveLanguage('js'), init.js);
     // This won't request JavaScript init
-    test.ok(init.resolveLanguage({
-      lang: 'something else'
-    }) !== init.js);
+    test.equal(init.resolveLanguage('something else'), null);
     test.done();
   },
-  rustArgs: function(test) {
+  rust: function(test) {
     test.expect(4);
     // No language arg does not mean Rust
-    test.ok(init.resolveLanguage({}) !== init.rs);
+    test.notEqual(init.resolveLanguage(), init.rs);
     // Can request Rust with explicit name
-    test.ok(init.resolveLanguage({
-      lang: 'rust'
-    }) === init.rs);
+    test.equal(init.resolveLanguage('rust'), init.rs);
     // Can request Rust with abbr
-    test.ok(init.resolveLanguage({
-      lang: 'rs'
-    }) === init.rs);
+    test.equal(init.resolveLanguage('rs'), init.rs);
     // This won't request Rust init
-    test.ok(init.resolveLanguage({
-      lang: 'whitespace'
-    }) !== init.rs);
+    test.equal(init.resolveLanguage('whitespace'), null);
     test.done();
   },
-  pythonArgs: function(test) {
+  python: function(test) {
     test.expect(4);
     // No language arg does not mean Rust
-    test.ok(init.resolveLanguage({}) !== init.py);
+    test.notEqual(init.resolveLanguage(), init.py);
     // Can request Rust with explicit name
-    test.ok(init.resolveLanguage({
-      lang: 'python'
-    }) === init.py);
+    test.equal(init.resolveLanguage('python'), init.py);
     // Can request Rust with abbr
-    test.ok(init.resolveLanguage({
-      lang: 'py'
-    }) === init.py);
+    test.equal(init.resolveLanguage('py'), init.py);
     // This won't request Rust init
-    test.ok(init.resolveLanguage({
-      lang: 'morse-code'
-    }) !== init.py);
+    test.equal(init.resolveLanguage('morse-code'), null);
     test.done();
   }
 };
 
-exports['init --lang rust'] = {
+exports['init --lang=rust'] = {
   setUp: function(done) {
     this.sandbox = sinon.sandbox.create();
-    this.warn = this.sandbox.stub(log, 'warn', function() {});
-    this.info = this.sandbox.stub(log, 'info', function() {});
+    this.warn = this.sandbox.stub(log, 'warn');
+    this.info = this.sandbox.stub(log, 'info');
+    this.copy = this.sandbox.stub(fs, 'copy', (source, target, callback) => {
+      callback();
+    });
     done();
   },
   tearDown: function(done) {
     this.sandbox.restore();
     done();
   },
+
   cargoVerifySucceed: function(test) {
     test.expect(3);
     // Stub our own exec so we don't try running cargo on the host cpu
@@ -83,17 +69,17 @@ exports['init --lang rust'] = {
     });
 
     // Stub the generating sample code so we don't write to fs
-    this.generateRustSample = this.sandbox.stub(init.rs, 'generateRustSample').returns(Promise.resolve());
+    this.createSampleProgram = this.sandbox.stub(init.rs, 'createSampleProgram').returns(Promise.resolve());
 
     // Attempt to initialize a Rust projec
-    init.initProject({
+    init.createNewProject({
         lang: 'rust'
       })
       // It should not succeed
       .then(() => {
         // Ensure exec was called just once
         test.equal(this.exec.callCount, 1);
-        test.equal(this.generateRustSample.callCount, 1);
+        test.equal(this.createSampleProgram.callCount, 1);
         test.done();
       })
       .catch(() => {
@@ -113,10 +99,10 @@ exports['init --lang rust'] = {
     });
 
     // Stub the generating sample code so we don't write to fs
-    this.generateRustSample = this.sandbox.stub(init.rs, 'generateRustSample').returns(Promise.resolve());
+    this.createSampleProgram = this.sandbox.stub(init.rs, 'createSampleProgram').returns(Promise.resolve());
 
     // Attempt to initialize a Rust projec
-    return init.initProject({
+    init.createNewProject({
         lang: 'rust'
       })
       // It should not succeed
@@ -127,32 +113,215 @@ exports['init --lang rust'] = {
         // Ensure exec was called just once
         test.equal(this.exec.callCount, 1);
         // Ensure we did not attempt to generate Rust code
-        test.equal(this.generateRustSample.callCount, 0);
+        test.equal(this.createSampleProgram.callCount, 0);
         // Ensure we received an error
         test.ok(err instanceof Error);
         test.done();
       });
-  }
+  },
+
+  'src/main.rs created': function(test) {
+    test.expect(5);
+
+    // Does not exist, will be created.
+    this.exists = this.sandbox.stub(fs, 'exists', (filename, callback) => {
+      callback(false);
+    });
+
+    this.mkdir = this.sandbox.stub(fs, 'mkdir', (dirname, callback) => {
+      callback(false);
+    });
+
+    init.rs.createSampleProgram()
+      .then(() => {
+        test.equal(this.copy.callCount, 2);
+        test.equal(this.copy.firstCall.args[0].endsWith(path.normalize('/resources/rust/Cargo.toml')), true);
+        test.equal(this.copy.firstCall.args[1].endsWith(path.normalize('Cargo.toml')), true);
+        test.equal(this.copy.lastCall.args[0].endsWith(path.normalize('/resources/rust/main.rs')), true);
+        test.equal(this.copy.lastCall.args[1].endsWith(path.normalize('/src/main.rs')), true);
+        test.done();
+      })
+      .catch(() => {
+        test.ok(false, 'a rejection should not be served if cargo is installed');
+      });
+  },
 };
 
-exports['init --lang javascript'] = {
+exports['init --lang=javascript'] = {
   setUp: function(done) {
     this.sandbox = sinon.sandbox.create();
-    this.warn = this.sandbox.stub(log, 'warn', function() {});
-    this.info = this.sandbox.stub(log, 'info', function() {});
+    this.warn = this.sandbox.stub(log, 'warn');
+    this.info = this.sandbox.stub(log, 'info');
+
+    this.writeFile = this.sandbox.stub(fs, 'writeFile', (filename, encoding, callback) => {
+      callback(null, {
+        stub: true,
+        filename
+      });
+    });
+    this.readFile = this.sandbox.stub(fs, 'readFile', (filename, encoding, callback) => {
+      callback(null, {
+        stub: true,
+        filename
+      });
+    });
+    this.copy = this.sandbox.stub(fs, 'copy', (src, dest, callback) => {
+      callback();
+    });
+
+
     done();
   },
   tearDown: function(done) {
     this.sandbox.restore();
     done();
   },
+
+
+  allOperations: function(test) {
+    test.expect(11);
+
+    // THIS TEST ONLY ASSERTS THAT ALL EXPECTED
+    // OPERATIONS ARE CALLED
+    var operations = [{
+      name: 'loadNpm',
+      callCount: 1,
+    }, {
+      name: 'resolveNpmConfig',
+      callCount: 1,
+    }, {
+      name: 'buildJSON',
+      callCount: 1,
+    }, {
+      name: 'prettyPrintJson',
+      callCount: 1,
+    }, {
+      name: 'writePackageJson',
+      callCount: 1,
+    }, {
+      name: 'readPackageJson',
+      callCount: 2,
+    }, {
+      name: 'getDependencies',
+      callCount: 1,
+    }, {
+      name: 'npmInstall',
+      callCount: 1,
+    }, {
+      name: 'createTesselinclude',
+      callCount: 1,
+    }, {
+      name: 'createSampleProgram',
+      callCount: 1,
+    }, ];
+
+    operations.forEach(operation => {
+      if (this[operation.name] && this[operation.name].restore) {
+        this[operation.name].restore();
+      }
+
+      this[operation.name] = this.sandbox.stub(init.js, operation.name, () => Promise.resolve('{"a":1}'));
+    });
+
+    init.js.generateProject({})
+      .then(() => {
+        test.ok(true);
+        operations.forEach(operation => {
+          test.equal(init.js[operation.name].callCount, operation.callCount);
+        });
+        test.done();
+      });
+  },
+
+  resolveNpmConfig: function(test) {
+    test.expect(1);
+
+    init.js.resolveNpmConfig({
+        config: {
+          list: {
+            'a': 1,
+            'b': 2,
+            'c': 3
+          }
+        }
+      })
+      .then((list) => {
+        test.deepEqual(list, {
+          a: 1,
+          b: 2,
+          c: 3
+        });
+        test.done();
+      });
+  },
+
+  createSampleProgram: function(test) {
+    test.expect(3);
+
+    this.exists = this.sandbox.stub(fs, 'exists', (filename, callback) => {
+      callback(false);
+    });
+
+
+    init.js.createSampleProgram()
+      .then(() => {
+        test.equal(this.copy.callCount, 1);
+        test.equal(this.copy.lastCall.args[0].endsWith(path.normalize('resources/javascript/index.js')), true);
+        test.equal(this.copy.lastCall.args[1].endsWith(path.normalize('index.js')), true);
+        test.done();
+      });
+  },
+
+  createSampleProgramExists: function(test) {
+    test.expect(1);
+
+    this.exists = this.sandbox.stub(fs, 'exists', (filename, callback) => {
+      callback(true);
+    });
+
+    init.js.createSampleProgram()
+      .then(() => {
+        test.equal(this.copy.callCount, 0);
+        test.done();
+      });
+  },
+
+  createTesselinclude: function(test) {
+    test.expect(3);
+
+    this.exists = this.sandbox.stub(fs, 'exists', (filename, callback) => {
+      callback(false);
+    });
+
+    init.js.createTesselinclude()
+      .then(() => {
+        test.equal(this.copy.callCount, 1);
+        test.equal(this.copy.lastCall.args[0].endsWith(path.normalize('resources/javascript/.tesselinclude')), true);
+        test.equal(this.copy.lastCall.args[1].endsWith(path.normalize('.tesselinclude')), true);
+        test.done();
+      });
+  },
+
+  createTesselincludeExists: function(test) {
+    test.expect(1);
+
+    this.exists = this.sandbox.stub(fs, 'exists', (filename, callback) => {
+      callback(true);
+    });
+
+    init.js.createTesselinclude()
+      .then(() => {
+        test.equal(this.copy.callCount, 0);
+        test.done();
+      });
+  },
 };
 
-exports['init --lang python'] = {
+exports['init --lang=python'] = {
   setUp: function(done) {
     this.sandbox = sinon.sandbox.create();
-    this.warn = this.sandbox.stub(log, 'warn', function() {});
-    this.info = this.sandbox.stub(log, 'info', function() {});
+    this.warn = this.sandbox.stub(log, 'warn');
+    this.info = this.sandbox.stub(log, 'info');
     done();
   },
   tearDown: function(done) {
