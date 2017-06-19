86e6249c134398d9632939df20c7486ba7638acc
Rick Waldron <waldron.rick@gmail.com>
2017-06-19 14:26:52 -0400

    tests: lib/tessel/deployment/javascript.js coverage
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- test/unit/fixtures/init/index.js
+++ test/unit/fixtures/init/index.js
@@ -1,8 +1,12 @@
-var tessel = require('tessel');
-
-tessel.led[2].on();
-
-setInterval(function() {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
+const tessel = require('tessel');
+const {
+  2: green,
+  3: blue
+} = tessel.led;
+green.on();
+setInterval(() => {
+  green.toggle();
+  blue.toggle();
 }, 100);
+
+console.log(`I'm blinking! (Press CTRL + C to stop)`);
